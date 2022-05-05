2cccc34309ea2f5e8199bf706f4aec0cf445c097
Nikita Pishchugin <nikita.pischugin@icloud.com>
2022-05-06 01:04:28 +0300

    add homework_app

---
+++ HomeWork_App/HomeWork_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork_App
+//
+//  Created by Nikita Pishchugin on 06.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
