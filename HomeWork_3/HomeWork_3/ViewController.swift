9195564a51185eb53d1c52a182c171f26b095146
Nikita <nikita.pishchugin@gmail.com>
2022-05-16 23:12:19 +0300

    complete HomeWork_3

--- HomeWork_3/HomeWork_3/ViewController.swift
+++ HomeWork_3/HomeWork_3/ViewController.swift
@@ -9,11 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var x = [(1, "x"), (4, "y"), (6, "a"), (-3, "b")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let xReady = x.map { Int(pow(Double($0.0), 2)) }.filter { $0 % 2 == 0 }.sorted { $0 > $1 }
+        print(xReady)
     }
 
-
 }
-
