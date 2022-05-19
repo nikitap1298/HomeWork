01007edaf0005e2270e2aa44fb33fdb2a62b515a
Nikita <nikita.pishchugin@gmail.com>
2022-05-19 23:21:29 +0300

    add german and czech beer

--- HomeWork_4/HomeWork_4/ViewController.swift
+++ HomeWork_4/HomeWork_4/ViewController.swift
@@ -17,6 +17,31 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+//        Pub.singleton.beer.name = "Pilsner Urquell"
+//        Pub.singleton.beer.price = 0.31
+//        Pub.singleton.beer.country = "Czech Republic"
+//        Pub.singleton.beer.remainingVolume = 39
+//        Pub.singleton.total = Pub.singleton.beer.remainingVolume
+        
+        beerName.text = "Beer:"
+        beerCountry.text = "Country:"
+        beerPrice.text = "Price:"
+        mainLabel.text = "Remaining: 0"
+    }
+    @IBAction func leftButton(_ sender: UIButton) {
+        Pub.singleton.beer.name = "Berliner Kindl"
+        Pub.singleton.beer.price = 0.42
+        Pub.singleton.beer.country = "Germany"
+        Pub.singleton.beer.remainingVolume = 45
+        Pub.singleton.total = Pub.singleton.beer.remainingVolume
+        
+        beerName.text = "Beer: \(Pub.singleton.beer.name)"
+        beerCountry.text = "Country: \(Pub.singleton.beer.country)"
+        beerPrice.text = "Price: \(Pub.singleton.beer.price) Euro for bottle"
+        mainLabel.text = "Remaining: \(Pub.singleton.remaining()) bottles"
+    }
+    
+    @IBAction func rightButton(_ sender: UIButton) {
         Pub.singleton.beer.name = "Pilsner Urquell"
         Pub.singleton.beer.price = 0.31
         Pub.singleton.beer.country = "Czech Republic"
