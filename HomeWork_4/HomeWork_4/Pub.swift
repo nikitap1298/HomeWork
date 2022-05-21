e8fc4b143b8b923efbe2a72fc0dde58c7c3da974
Nikita <nikita.pishchugin@gmail.com>
2022-05-21 23:13:03 +0300

    fixes

--- HomeWork_4/HomeWork_4/Pub.swift
+++ HomeWork_4/HomeWork_4/Pub.swift
@@ -13,8 +13,11 @@ class Pub {
     var beer: [Beer] = []
     
     var sell0: Int = 0
+    var earn0: Double = 0
     var sell1: Int = 0
+    var earn1: Double = 0
     var sell2: Int = 0
+    var earn2: Double = 0
     var isWorking: Int = 0
     
     private init(beer: [Beer]) {
@@ -22,35 +25,41 @@ class Pub {
     }
     
     func sellOne() -> String {
-        if Pub.singleton.beer[0].remainingVolume >= 1 && isWorking == 0 {
-            Pub.singleton.beer[0].remainingVolume -= 1
+        if isWorking == 0 {
+            Pub.singleton.beer[isWorking].remainingVolume -= 1
             sell0 += 1
-        } else if Pub.singleton.beer[1].remainingVolume >= 1 && isWorking == 1 {
-            Pub.singleton.beer[1].remainingVolume -= 1
+            earn0 = Double(sell0) * Pub.singleton.beer[isWorking].price
+        } else if isWorking == 1 {
+            Pub.singleton.beer[isWorking].remainingVolume -= 1
             sell1 += 1
-        } else if Pub.singleton.beer[2].remainingVolume >= 1 && isWorking == 2 {
-            Pub.singleton.beer[2].remainingVolume -= 1
+            earn1 = Double(sell1) * Pub.singleton.beer[isWorking].price
+        } else if isWorking == 2 {
+            Pub.singleton.beer[isWorking].remainingVolume -= 1
             sell2 += 1
+            earn2 = Double(sell2) * Pub.singleton.beer[isWorking].price
         }
         return String("\(Pub.singleton.beer[0].remainingVolume); \(Pub.singleton.beer[1].remainingVolume); \(Pub.singleton.beer[2].remainingVolume)")
     }
     
     
     func earning() -> Double {
-        let earn = (Double(sell0) * beer[0].price) + (Double(sell1) * beer[1].price) + (Double(sell2) * beer[2].price)
+        let earn = earn0 + earn1 + earn2
         let earnTwoDigits = Double(round(1000 * earn)) / 1000
         return earnTwoDigits
     }
     
     func resetEarning() -> Double {
-        sell0 = 0
-        sell1 = 0
-        sell2 = 0
+        earn0 = 0
+        earn1 = 0
+        earn2 = 0
         return earning()
     }
     
     func checkRemaining() -> String {
-        return sellOne()
+        sell0 = 0
+        sell1 = 0
+        sell2 = 0
+        return String("\(Pub.singleton.beer[0].remainingVolume); \(Pub.singleton.beer[1].remainingVolume); \(Pub.singleton.beer[2].remainingVolume)")
     }
     
 }
