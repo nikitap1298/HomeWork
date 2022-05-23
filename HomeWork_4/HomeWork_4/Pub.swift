a62f0485e2dc45aa5da2149ad955eb5170850a89
Nikita <nikita.pishchugin@gmail.com>
2022-05-23 13:43:15 +0300

    small fixes

--- HomeWork_4/HomeWork_4/Pub.swift
+++ HomeWork_4/HomeWork_4/Pub.swift
@@ -21,12 +21,15 @@ class Pub {
             Pub.singleton.beer[n].remainingVolume -= 1
             Pub.singleton.beer[n].total += 1
         }
-        return beer.map { $0.remainingVolume }.toPrint
+        return beer.map { ($0.name, $0.remainingVolume) }.toPrint
     }
     
-    
     func earning() -> Double {
+        
+        // Convert beer array to output only earning for each Beer. Example: [0.39, 0.31, 0.28]
         let earnEach = beer.map { Double($0.total) * $0.price }
+        
+        // Sum all elements in earnEachArray
         let eachSum = earnEach.reduce(0) { (sum, num) -> Double in
             sum + num
         }
@@ -35,6 +38,8 @@ class Pub {
     }
     
     func resetEarning() -> Double {
+        
+        // Reset total value to 0
         beer = beer.map { Beer(name: $0.name, price: $0.price, country: $0.country, remainingVolume: $0.remainingVolume, total: $0.total * 0 ) }
         return earning()
     }
