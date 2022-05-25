9cac565fd3b74f703ef902002307f80b64e9f8e0
Nikita <nikita.pishchugin@gmail.com>
2022-05-25 23:15:49 +0300

    complete catToy

--- HomeWork_5/HomeWork_5/ViewController.swift
+++ HomeWork_5/HomeWork_5/ViewController.swift
@@ -8,48 +8,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+
     let catToy = UIView()
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            
+            catToy.backgroundColor = .red
+        }
         
-        catToy.backgroundColor = .red
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
+        override func viewWillAppear(_ animated: Bool) {
+            super.viewWillAppear(animated)
+            
+            let xCenter = view.frame.midX
+            let yCenter = view.frame.midY
+            
+            catToy.frame = CGRect(x: xCenter - 100 / 2, y: yCenter - 100 / 2, width: 100, height: 100)
+            catToy.layer.cornerRadius = 50
+            view.addSubview(catToy)
+            
+            catToy.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(didTap)))
+            
+        }
         
-        let xCenter = view.frame.midX
-        let yCenter = view.frame.midY
+        @objc func didTap() {
+            randomPlace()
+            randomColor()
+        }
         
-        catToy.frame = CGRect(x: xCenter - 100 / 2, y: yCenter - 100 / 2, width: 100, height: 100)
-        catToy.layer.cornerRadius = 50
-        view.addSubview(catToy)
+        func randomPlace() {
+            self.catToy.frame = CGRect(x: .random(in: 0..<view.frame.maxX - 15), y: .random(in: 0..<view.frame.maxY - 15), width: 100, height: 100)
+        }
         
-        catToy.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(didTap)))
-        
-    }
-    
-    @objc func didTap() {
-        randomPlace()
-        randomColor()
-    }
-    
-    func randomPlace() {
-        self.catToy.frame = CGRect(x: .random(in: 0..<view.frame.maxX - 15), y: .random(in: 0..<view.frame.maxY - 15), width: 100, height: 100)
-    }
-    
-    func randomColor() {
-        let redR = CGFloat.random(in: 0...1)
-        let greenR = CGFloat.random(in: 0...1)
-        let blueR = CGFloat.random(in: 0...1)
-        let randomColor = UIColor(red: redR, green: greenR, blue: blueR, alpha: 1)
-        
-        catToy.backgroundColor = randomColor
-    }
-    
-    
-    
+        func randomColor() {
+            let redR = CGFloat.random(in: 0...1)
+            let greenR = CGFloat.random(in: 0...1)
+            let blueR = CGFloat.random(in: 0...1)
+            let randomColor = UIColor(red: redR, green: greenR, blue: blueR, alpha: 1)
+            
+            catToy.backgroundColor = randomColor
+        }
+
 }
 
