98a93ca33b9f12a77317fc4330e2c49f72fad0db
Nikita <nikita.pishchugin@gmail.com>
2022-05-25 21:22:11 +0300

    create HomeWork_5

---
+++ HomeWork_5/HomeWork_5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork_5
+//
+//  Created by Nikita Pishchugin on 25.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
