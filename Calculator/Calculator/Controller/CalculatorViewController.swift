2bd05a42e59a172495580004d1acc70a30642cff
Nikita <nikita.pishchugin@gmail.com>
2022-06-09 15:03:49 +0300

    finish calculator

--- Calculator/Calculator/Controller/CalculatorViewController.swift
+++ Calculator/Calculator/Controller/CalculatorViewController.swift
@@ -10,43 +10,57 @@ import UIKit
 class CalculatorViewController: UIViewController {
     
     // MARK: - Private Properties
-    private var isTyping = false
+    
+    // Helps to understand that new number is entering
+    private var isTyping: Bool = false
+    
+    // First operand (number before user press math button)
     private var firstNumber: Double = 0
+    
+    // Second operand (number after user press math button)
     private var secondNumber: Double = 0
     private var sign: String = ""
+    private var commaPlaced: Bool = false
     
+    // Computed Property. Transform String into Double
     private var currentValue: Double {
         get {
             return Double(valueLabel.text!)!
         }
         set {
-            valueLabel.text = String(newValue)
+            let value = "\(newValue)"
+            let valueArray = value.components(separatedBy: ".")
+            if valueArray[1] == "0" {
+                valueLabel.text = "\(valueArray[0])"
+            } else {
+                valueLabel.text = "\(newValue)"
+            }
             isTyping = false
         }
     }
     
-    // MARK: - Public Properties
-    
     // MARK: - IBOutlet
     @IBOutlet weak var valueLabel: UILabel!
+    @IBOutlet var buttonsLabel: [UIButton]!
     
     // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Makes each button round
+        for i in buttonsLabel {
+            i.layer.cornerRadius = i.frame.height / 2
+        }
     }
     
     // MARK: - IBActions
-    @IBAction func simpleMathButton(_ sender: UIButton) {
-        sign = sender.currentTitle!
-        firstNumber = currentValue
-        isTyping = false
-    }
-    
     @IBAction func numberButton(_ sender: UIButton) {
         let number = sender.currentTitle!
         
+        // If isTyping = true then we add new digit on the screen
         if isTyping {
+            
+            // Max 10 digits for label
             if valueLabel.text!.count < 10 {
                 valueLabel.text = valueLabel.text! + number
             }
@@ -58,28 +72,66 @@ class CalculatorViewController: UIViewController {
         }
         
     }
+    @IBAction func simpleMathButton(_ sender: UIButton) {
+        sign = sender.currentTitle!
+        firstNumber = currentValue
+        
+        // When user press " +, -, *, / " programm understand that user stop entering digit
+        isTyping = false
+        commaPlaced = false
+    }
     
     @IBAction func equalButton(_ sender: UIButton) {
         if isTyping {
             secondNumber = currentValue
         }
         
+        commaPlaced = false
+
         switch sign {
         case "÷":
-            simpleMath {$0 / $1}
+            currentValue = firstNumber / secondNumber
+            isTyping = false
         case "✕":
-            simpleMath {$0 * $1}
-        case "-":
-            simpleMath {$0 - $1}
+            currentValue = firstNumber * secondNumber
+            isTyping = false
+        case "−":
+            currentValue = firstNumber - secondNumber
+            isTyping = false
         default:
-            simpleMath {$0 + $1}
+            currentValue = firstNumber + secondNumber
+            isTyping = false
         }
     }
     
-    // MARK: - Private Functions
-    private func simpleMath(operation: (Double, Double) -> Double) {
-        currentValue = operation(firstNumber, secondNumber)
-        isTyping = false
+    @IBAction func clearButton(_ sender: UIButton) {
+        switch sender.currentTitle {
+        case "AC":
+            firstNumber = 0
+            secondNumber = 0
+            currentValue = 0
+            valueLabel.text = "0"
+            isTyping = false
+            commaPlaced = false
+            sign = ""
+        case "+/-":
+            currentValue = -currentValue
+        case "%":
+            if firstNumber == 0 {
+                currentValue = currentValue / 100
+            } else {
+                currentValue = firstNumber / 100
+            }
+            isTyping = false
+        default:
+            if isTyping && commaPlaced == false {
+                valueLabel.text = valueLabel.text! + "."
+                commaPlaced = true
+            } else if isTyping == false && commaPlaced == false  {
+                valueLabel.text = "0."
+            }
+        }
     }
+    
 }
 
