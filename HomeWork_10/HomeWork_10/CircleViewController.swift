3d3e776ae03519635d87c132209c19dc48b4b4ab
Nikita <nikita.pishchugin@gmail.com>
2022-06-14 12:46:04 +0300

    init HomeWork_10

---
+++ HomeWork_10/HomeWork_10/CircleViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork_10
+//
+//  Created by Nikita Pishchugin on 14.06.2022.
+//
+
+import UIKit
+
+class CircleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
