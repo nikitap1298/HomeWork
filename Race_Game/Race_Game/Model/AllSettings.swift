2a3f51e6eb79902d74370c7bf722a63badd46fe5
Nikita <nikita.pishchugin@gmail.com>
2022-07-13 18:57:03 +0300

    delete name from class AllSettings

--- Race_Game/Race_Game/Model/AllSettings.swift
+++ Race_Game/Race_Game/Model/AllSettings.swift
@@ -8,17 +8,15 @@
 import UIKit
 
 class AllSettings {
-    static let singleton = AllSettings(carColor: UIColor.systemYellow, barrier: UIImage(named: "Grass_Image")!, name: "Nikita", speed: 2.5)
+    static let singleton = AllSettings(carColor: UIColor.systemYellow, barrier: UIImage(named: "Grass_Image")!, speed: 2.5)
     
     var carColor: UIColor
     var barrier: UIImage
-    var name: String
     var speed: Double
     
-    private init(carColor: UIColor, barrier: UIImage, name: String, speed: Double) {
+    private init(carColor: UIColor, barrier: UIImage, speed: Double) {
         self.carColor = carColor
         self.barrier = barrier
-        self.name = name
         self.speed = speed
     }
 }
