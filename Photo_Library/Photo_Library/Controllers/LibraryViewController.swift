5a9b08898a7422fcfaef707ba266400aa3d2e20a
Nikita <nikita.pishchugin@gmail.com>
2022-07-19 14:02:36 +0300

    add maxLength for UITextField

--- Photo_Library/Photo_Library/Controllers/LibraryViewController.swift
+++ Photo_Library/Photo_Library/Controllers/LibraryViewController.swift
@@ -280,4 +280,12 @@ extension LibraryViewController: UITextFieldDelegate {
         textField.resignFirstResponder()
         return true
     }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let maxLenght = 30
+        let currentString = (textField.text ?? "") as NSString
+        let newString = currentString.replacingCharacters(in: range, with: string)
+        
+        return newString.count <= maxLenght
+    }
 }
